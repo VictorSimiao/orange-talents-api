59769a9c5e3b87ebce587ecc9a52bb37ff5ac9cf
Victor Simiao <victor.sreis@hotmail.com>
2021-01-15 20:53:10 -0300

    Iniciando Projeto

---
+++ src/main/java/br/com/orange/talents/api/OrangeTalentsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.orange.talents.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeTalentsApiApplication.class, args);
+	}
+
+}
